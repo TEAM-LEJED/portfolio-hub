a85d9d43ccaadef306775031838bbae636e8423a
elizabeth-barden <bethheal9@gmail.com>
2024-07-09 14:40:30 +0000

    Created Routes

--- src/layouts/rootLayout.jsx
+++ src/layouts/rootLayout.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Navbar from '../globalComponents/navbar'
-import Footer from '../globalComponents/footer'
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
     <div>
-    <Navbar/>
     <Outlet/>
     <Footer/>
     </div>
