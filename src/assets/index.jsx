1aea62b2cf6767927bff8c3ccd1edc10102a1d25
elizabeth-barden <bethheal9@gmail.com>
2024-07-11 12:05:50 +0000

    assets add

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 
-const index = () => {
-  return (
-    <div>index</div>
-  )
-}
+import SignUp from "./images/signUp.jpg"
+import fieldImage from "./images/fieldImg.jpg"
+import googleIcon from "./images/google.png"
+import linkedinIcon from "./images/linkedin.png"
 
-export default index
+
+
+export {SignUp,fieldImage,googleIcon,linkedinIcon} 
