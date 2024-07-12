7a5f0bf37723c2f2f947496ad5fce0e88f1954fa
elizabeth-barden <bethheal9@gmail.com>
2024-07-12 14:53:59 +0000

    login page

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,37 +1,34 @@
 import React from 'react';
-import { ChevronRight, Lock, User } from 'lucide-react';
-import ToggleSwitch from './toggleSwitch';
+import { Lock, User } from 'lucide-react';
+
 const LoginForm = () => {
   return (
-
-      <form className="flex flex-col justify-center items-center mt-24">
-        <div>LOG IN </div>
-     <div className="relative mb-4">
+    <form className="flex flex-col justify-center items-center mt-10 mb-8">
+      <div className="relative mb-4">
         <User className="absolute text-[#FCC73F]" />
-         <input
-           type="text"
-          className="border-b-2 border-[#FCC73F] bg-transparent px-4  w-3/4 transition duration-200"
-         placeholder="username"
-         />
+        <input
+          type="text"
+          className="border-b-2 border-[#FCC73F] bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="username"
+        />
       </div>
-       <div className="relative mb-4">
+      <div className="relative mb-4">
         <Lock className="absolute text-[#FCC73F]" />
         <input
-           type="password"
-           className="border-b-2 border-[#FCC73F] bg-transparent px-4  w-3/4 transition duration-200"
+          type="password"
+          className="border-b-2 border-[#FCC73F] bg-transparent px-4 w-3/4 transition duration-200"
           placeholder="Password"
-         />
-       </div>
-       <button className="button rounded-full w-40 h-10 bg-[#3D5A5E] text-[#FCC73F] uppercase font-bold shadow-md hover:border-purple-600 hover:outline-none transition duration-200">
-       <span>Log In </span>
-
-
-     </button>
-     <ToggleSwitch/>
-
-
+        />
+      </div>
+     
+      <button
+        type="submit"
+        className="button rounded-full w-40 h-10 bg-[#3D5A5E] text-[#FCC73F] uppercase font-bold shadow-md hover:border-purple-600 hover:outline-none transition duration-200"
+      >
+        <span>Log In</span>
+      </button>
     </form>
   );
-}
+};
 
 export default LoginForm;
