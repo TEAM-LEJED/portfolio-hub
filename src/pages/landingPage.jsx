88716e66cf6b28765f3e2353f3cf70d9f807366a
Lyn-Dah <edulind99@gmail.com>
2024-07-15 09:30:39 +0000

    Add landing page

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/navbar';
+
 
 const LandingPage = () => {
   return (
     <div>
-    <div>LandingPage</div>
-    <div>LogIn</div>
-    <div>SignUp</div>
+      <Navbar />
+      
+        <div className="container">
+          <h1>Welcome to LEJED PORTFOLIO</h1>
+          <p>Please login or sign up to continue.</p>
+          <Link to="/login" className="btn">Login</Link>
+          <Link to="/signup" className="btn">Sign Up</Link>
+        </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default LandingPage
+export default LandingPage;
