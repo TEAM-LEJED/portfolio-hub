7b332c21c79961a4f3841680e9bc46498f8e6572
Lyn-Dah <edulind99@gmail.com>
2024-07-15 05:07:42 +0000

    Add dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+
 import LandingPage from './pages/landingPage'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import SignupPage from './pages/signupPage'
 import LoginPage from './pages/loginPage'
 import Resume from './pages/dashBoard/pages/resume'
-import Experience from './pages/dashBoard/pages/experience'
 import Projects from './pages/dashBoard/pages/projects'
-import Overview from './pages/dashBoard/pages/overview'
 import DashBoardLayout from './pages/dashBoard/layout'
 import Skills from './pages/dashBoard/pages/Skills'
+import Achievement from './pages/dashBoard/pages/achievement'
+import Education from './pages/dashBoard/pages/education'
+import Volunteering from './pages/dashBoard/pages/volunteering'
+import Overview from './pages/dashBoard/pages/overview'
+import AddProject from './pages/dashBoard/pages/addProject'
+import AddExperience from './pages/dashBoard/pages/addExperience'
+import AddSkill from './pages/dashBoard/pages/addSkill'
+import AddResume from './pages/dashBoard/pages/addResume'
+import Experiences from './pages/dashBoard/pages/experiences'
 
 const App = () => {
   const router = createBrowserRouter([
@@ -38,20 +45,47 @@ const App = () => {
   element:<Resume/>
 },
 {
+  path: "resume/add-resume",
+  element: <AddResume/>
+  },
+{
   path : "skills",
   element:<Skills/>
 },
 {
-  path : "project",
+  path: "skills/add-skill",
+  element: <AddSkill/>
+  },
+{
+  path : "projects",
   element:<Projects/>
 },
 {
-  path : "experience",
-  element:<Experience/>
+  path: "projects/add-project",
+  element: <AddProject/>
+  },
+
+{
+  path : "experiences",
+  element:<Experiences/>
+},
+
+{
+  path: "experiences/add-experience",
+  element: <AddExperience/>
+  },
+
+{
+  path: "achievement",
+  element: <Achievement/>
 },
 {
-  path: "overview",
-  element: <Overview/>
+  path: "education",
+  element: <Education/>
+},
+{
+  path: "volunteering",
+  element: <Volunteering/>
 }
   ]
 },
