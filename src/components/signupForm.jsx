eb3fe3d53f16d2d854bc1f4ff85fd1f43c3cb5d2
elizabeth-barden <bethheal9@gmail.com>
2024-08-15 10:14:08 +0000

    changes

--- src/components/signupForm.jsx
+++ src/components/signupForm.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Lock, User } from 'lucide-react';
-import { useForm } from 'react-hook-form';
-import { apiCheckUsernameExists, apiSignup } from '../services/auth';
-import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
-import Loader from './preview/loader';
-import { debounce } from 'lodash';
+import React, { useEffect, useState } from "react";
+import { Lock, User } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { apiCheckUsernameExists, apiSignup } from "../services/auth";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import Loader from "./preview/loader";
+import { debounce } from "lodash";
 
 const SignupForm = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -22,46 +22,37 @@ const SignupForm = () => {
   const checkUserName = async (userName) => {
     setIsUsernameLoading(true);
     try {
-      const res = await apiCheckUsernameExists(userName)
+      const res = await apiCheckUsernameExists(userName);
       console.log(res.data);
-      const user = await res.data.user
+      const user = await res.data.user;
       if (user) {
-        setUsernameNotAvailable(true)
-        setUsernameAvailable(false)
+        setUsernameNotAvailable(true);
+        setUsernameAvailable(false);
       } else {
-        setUsernameAvailable(true)
-        setUsernameNotAvailable(false)
+        setUsernameAvailable(true);
+        setUsernameNotAvailable(false);
       }
     } catch (error) {
       console.log(error);
       toast.error("An error occurred. Please try again.!");
-
-    }
-    finally {
+    } finally {
       setIsUsernameLoading(false);
     }
-  }
+  };
 
-  const userNameWatch = watch('userName')
-  
+  const userNameWatch = watch("userName");
 
   useEffect(() => {
-    const debouncedSearch = debounce( async()=> {
+    const debouncedSearch = debounce(async () => {
       if (userNameWatch) {
-       await checkUserName(userNameWatch);
+        await checkUserName(userNameWatch);
       }
     }, 1000);
     debouncedSearch();
     return () => debouncedSearch.cancel();
-
   }, [userNameWatch]);
-    
-
-
-  
 
   const onSubmit = async (data) => {
-   
     setIsSubmitting(true);
     let payload = {
       firstName: data.firstName,
@@ -69,8 +60,8 @@ const SignupForm = () => {
       userName: data.userName,
       email: data.email,
       password: data.password,
-      confirmPassword: data.confirmPassword
-    }
+      confirmPassword: data.confirmPassword,
+    };
     if (data.otherNames) {
       payload = { ...payload, otherNames: data.otherNames };
     }
@@ -89,124 +80,133 @@ const SignupForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center items-center mt-2 mb-8">
-  <div className="relative mb-4 w-3/4">
-    <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
-    <input
-      id="First Name"
-      type="text"
-      className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
-      placeholder="First Name "
-      {...register("firstName", { required: "First Name required" })}
-    />
-    {errors.firstName && (
-      <p className="text-red-500">{errors.firstName.message}</p>
-    )}
-  </div>
-  
-  <div className="relative mb-4 w-3/4">
-    <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-" />
-    <input
-      id="Last Name"
-      type="text"
-      className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
-      placeholder="Last Name"
-      {...register("lastName", { required: "Last Name required" })}
-    />
-    {errors.lastName && (
-      <p className="text-red-500">{errors.lastName.message}</p>
-    )}
-  </div>
-  
-  <div className="relative mb-4 w-3/4">
-    <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
-    <input
-      id="Other Name"
-      type="text"
-      className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
-      placeholder="Other Name"
-      {...register("OtherName")}
-    />
-  </div>
-  
-  <div className="relative mb-4 w-3/4">
-    <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
-    <input
-      id="username"
-      type="text"
-      className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
-      placeholder="username"
-      {...register("userName", {
-        required: "username required",
-        minLength: { value: 6, message: "username must be at least 6 characters" },
-      })}
-    />
-    {errors.userName && (
-      <p className="text-red-500">{errors.userName.message}</p>
-    )}
-    <div className="flex items-center gap-x-2">
-      {isUsernameLoading && <Loader />}
-      {usernameAvailable && <p className="text-green-500">Username available!</p>}
-      {usernameNotAvailable && <p className="text-red-500">Username not available!</p>}
-    </div>
-  </div>
-  
-  <div className="relative mb-4 w-3/4">
-    <Lock className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
-    <input
-      id="email"
-      type="email"
-      className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
-      placeholder="email"
-      {...register("email", { required: "email required" })}
-    />
-    {errors.email && (
-      <p className="text-red-500">{errors.email.message}</p>
-    )}
-  </div>
-  
-  <div className="relative mb-4 w-3/4">
-    <Lock className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
-    <input
-      id="password"
-      type="password"
-      className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
-      placeholder="Password"
-      {...register("password", {
-        required: "Password is required",
-        minLength: {
-          value: 8,
-          message: "Password length must be more than 8 characters",
-        },
-      })}    
-      />
-    {errors.password && (
-      <p className="text-red-500">{errors.password.message}</p>
-    )}
-  </div>
-  
-  <div className="relative mb-4 w-3/4">
-    <Lock className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
-    <input
-      id="confirmPassword"
-      type="password"
-      className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
-      placeholder="confirm password"
-      {...register("confirmPassword", { required: "password doesn't match" })}
-    />
-    {errors.confirmPassword && (
-      <p className="text-red-500">{errors.confirmPassword.message}</p>
-    )}
-  </div>
-  
-  <button
-    type="submit"
-    className="rounded-full w-40 h-10 bg-[#12071F] text-white uppercase font-bold shadow-md hover:border-purple-600 hover:outline-none transition duration-200"
-  >
-    Sign Up
-  </button>
-</form>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col justify-center items-center mt-2 mb-8"
+    >
+      <div className="relative mb-4 w-3/4">
+        <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
+        <input
+          id="First Name"
+          type="text"
+          className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="First Name "
+          {...register("firstName", { required: "First Name required" })}
+        />
+        {errors.firstName && (
+          <p className="text-red-500">{errors.firstName.message}</p>
+        )}
+      </div>
 
+      <div className="relative mb-4 w-3/4">
+        <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-" />
+        <input
+          id="Last Name"
+          type="text"
+          className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="Last Name"
+          {...register("lastName", { required: "Last Name required" })}
+        />
+        {errors.lastName && (
+          <p className="text-red-500">{errors.lastName.message}</p>
+        )}
+      </div>
+
+      <div className="relative mb-4 w-3/4">
+        <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
+        <input
+          id="Other Name"
+          type="text"
+          className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="Other Name"
+          {...register("OtherName")}
+        />
+      </div>
+
+      <div className="relative mb-4 w-3/4">
+        <User className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
+        <input
+          id="username"
+          type="text"
+          className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="username"
+          {...register("userName", {
+            required: "username required",
+            minLength: {
+              value: 6,
+              message: "username must be at least 6 characters",
+            },
+          })}
+        />
+        {errors.userName && (
+          <p className="text-red-500">{errors.userName.message}</p>
+        )}
+        <div className="flex items-center gap-x-2">
+          {isUsernameLoading && <Loader />}
+          {usernameAvailable && (
+            <p className="text-green-500">Username available!</p>
+          )}
+          {usernameNotAvailable && (
+            <p className="text-red-500">Username not available!</p>
+          )}
+        </div>
+      </div>
+
+      <div className="relative mb-4 w-3/4">
+        <Lock className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
+        <input
+          id="email"
+          type="email"
+          className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="email"
+          {...register("email", { required: "email required" })}
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+      </div>
+
+      <div className="relative mb-4 w-3/4">
+        <Lock className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
+        <input
+          id="password"
+          type="password"
+          className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="Password"
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password length must be more than 8 characters",
+            },
+          })}
+        />
+        {errors.password && (
+          <p className="text-red-500">{errors.password.message}</p>
+        )}
+      </div>
+
+      <div className="relative mb-4 w-3/4">
+        <Lock className="absolute left-0 top-1/2 transform -translate-y-1/2 text-[#12071F]" />
+        <input
+          id="confirmPassword"
+          type="password"
+          className="border-b-2 border-white bg-transparent px-4 w-3/4 transition duration-200"
+          placeholder="confirm password"
+          {...register("confirmPassword", {
+            required: "password doesn't match",
+          })}
+        />
+        {errors.confirmPassword && (
+          <p className="text-red-500">{errors.confirmPassword.message}</p>
+        )}
+      </div>
+
+      <button
+        type="submit"
+        className="rounded-full w-40 h-10 bg-[#12071F] text-white uppercase font-bold shadow-md hover:border-purple-600 hover:outline-none transition duration-200"
+      >
+        Sign Up
+      </button>
+    </form>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Bounce, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
